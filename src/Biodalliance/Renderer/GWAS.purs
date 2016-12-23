f693f1501a9cd123806c65ddacac6b351c33ad68
Christian Fischer <christian@chfi.se>
2016-12-23 11:40:37 +0100

    Start GWAS track renderer

---
+++ src/Biodalliance/Renderer/GWAS.purs
@@ -0,0 +1,37 @@
+module Biodalliance.Renderer.GWAS
+       ( renderTier
+       , drawTier
+       ) where
+
+import Prelude
+
+import Control.Monad.Eff (Eff)
+import Data.Function.Uncurried (Fn1, Fn2, mkFn1, mkFn2, runFn1)
+import Graphics.Canvas (CANVAS, setStrokeStyle)
+
+import Biodalliance.Glyph (Glyph, circle, flattenGlyphs, logScale)
+import Biodalliance.Track (Tier, Feature)
+import Biodalliance.Track as Track
+
+type GWASFeature = Feature ( pValue :: Number )
+
+gwasGlyph :: forall eff. GWASFeature -> Glyph Unit eff
+gwasGlyph { min, max, pValue } = circle { x: min, y: pValue } 3.0
+
+gwasPlotGlyph :: forall eff. Tier -> Glyph Unit eff
+gwasPlotGlyph tier = flattenGlyphs $ map gwasGlyph (Track.tierFeatures tier)
+
+drawGwasPlot :: forall eff. Tier -> Eff (canvas :: CANVAS | eff) Unit
+drawGwasPlot tier = do
+  pure $ Track.setTierHeight tier 500.0
+  let sf = Track.tierScaleFactor tier logScale
+      ctx = Track.tierCanvasContext tier
+  setStrokeStyle "#222222" ctx
+  gwasPlotGlyph tier sf ctx
+
+
+renderTier :: forall eff. Fn2 String Tier (Eff (canvas :: CANVAS | eff) Unit)
+renderTier = mkFn2 \status tier -> Track.runEff $ runFn1 drawTier tier
+
+drawTier :: forall eff. Fn1 Tier (Eff (canvas :: CANVAS | eff) Unit)
+drawTier = mkFn1 drawGwasPlot
