577689df91f5ebef8f681ad4c953af3301fe09cb
Christian Fischer <christian@chfi.se>
2017-03-02 14:07:35 +0100

    Add visual test of feature translation and scaling

--- test/Main.purs
+++ test/Main.purs
@@ -1,11 +1,10 @@
 module Test.Main where
 
-import Prelude
-import Biodalliance.Glyph.Free.Canvas as Canvas
-import Biodalliance.Glyph.Free.SVG as SVG
-import Test.QuickCheck.Laws.Data as Data
+import Biodalliance.Feature
 import Biodalliance.Glyph.Free (Glyph, circle, fill, line, rect, stroke)
+import Biodalliance.Glyph.Free.Canvas as Canvas
 import Biodalliance.Glyph.Free.Interpret (writeGlyph)
+import Biodalliance.Glyph.Free.SVG as SVG
 import Biodalliance.Glyph.Position (GlyphPosition(..))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
@@ -18,8 +17,10 @@ import Data.Foreign (Foreign)
 import Data.Maybe (Maybe(..))
 import Data.Traversable (traverse, traverse_)
 import Debug.Trace (traceAny)
-import Graphics.Canvas (getCanvasElementById, getContext2D)
+import Graphics.Canvas (getCanvasElementById, getContext2D, translate, TranslateTransform(..))
+import Prelude
 import Test.QuickCheck.Laws (QC)
+import Test.QuickCheck.Laws.Data as Data
 import Type.Proxy (Proxy(..))
 
 foreign import testGlyphPos :: Foreign -> String
@@ -41,8 +42,24 @@ exGlyph = do
   rect {x: 0.0, y: 10.0} {x: 400.0, y: 15.0}
 
 
-bdGlyph :: Foreign
-bdGlyph = writeGlyph {min: 0.0, max: 100.0} exGlyph
+exFeature1 :: Feature ()
+exFeature1 = Feature { min: -5.0, max: 5.0 }
+exFeature2 :: Feature ()
+exFeature2 = Feature { min: 10.0, max: 20.0 }
+exFeature3 :: Feature ()
+exFeature3 = Feature { min: 600.0, max: 610.0 }
+
+glyphify :: Number -> Feature () -> Glyph Unit
+glyphify y (Feature f) = do
+  stroke "#ff0000"
+  fill "#555555"
+  rect { x: f.min, y: y } { x: f.max, y: y + 40.0 }
+
+
+glyph1 = glyphify 0.0 (translateFeature 0.0 (scaleFeature 5.0 exFeature1))
+glyph2 = glyphify 50.0 (translateFeature 0.0 (scaleFeature 5.0 exFeature2))
+glyph3 = glyphify 100.0 (translateFeature 0.0 (scaleFeature 5.0 exFeature3))
+
 
 checkGlyphPosInstances :: ∀ e. QC e Unit
 checkGlyphPosInstances = do
@@ -58,11 +75,12 @@ runBrowserTest = do
     Nothing -> log "couldn't find canvas"
     Just c  -> do
       ctx <- getContext2D c
+      translate {translateX: -200.0, translateY: 0.0} ctx
       log "rendering glyph to canvas"
-      Canvas.renderGlyph ctx exGlyph
+      traverse_ (Canvas.renderGlyph ctx) [glyph1, glyph2, glyph3]
 
   log "rendering glyph to svg"
-  SVG.renderGlyph exGlyph >>= addElementToDiv "svgDiv"
+  traverse_ (\g -> SVG.renderGlyph g >>= addElementToDiv "svgDiv") [glyph1, glyph2, glyph3]
 
   pure unit
 
