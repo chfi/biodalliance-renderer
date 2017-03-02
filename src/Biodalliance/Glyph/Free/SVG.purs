bed5fdeb60c4ff4c133d9125ab536b52ebf6254b
Christian Fischer <christian@chfi.se>
2017-03-02 12:34:42 +0100

    Consistent all-uppercase SVG

--- src/Biodalliance/Glyph/Free/SVG.purs
+++ src/Biodalliance/Glyph/Free/SVG.purs
@@ -13,25 +13,25 @@ import Control.Monad.Writer (execWriter)
 import DOM (DOM)
 import DOM.Node.Types (Element)
 
-interpSvgEff :: GlyphF ~> SVG
-interpSvgEff (Stroke c a)  = do
+interpSVGEff :: GlyphF ~> SVG
+interpSVGEff (Stroke c a)  = do
   SVG.setStrokeStyle c
   pure a
-interpSvgEff (Fill c a)     = do
+interpSVGEff (Fill c a)     = do
   SVG.setFillStyle c
   pure a
-interpSvgEff (Circle p r a) = do
+interpSVGEff (Circle p r a) = do
   SVG.circle p.x p.y r
   pure a
-interpSvgEff (Line p1 p2 a) = do
+interpSVGEff (Line p1 p2 a) = do
   SVG.line p1.x p1.y p2.x p2.y
   pure a
-interpSvgEff (Rect p1 p2 a) = do
+interpSVGEff (Rect p1 p2 a) = do
   SVG.rect p1.x p1.y p2.x p2.y
   pure a
 
-runSvgEff :: ∀ a. Glyph a -> Array SVGElement
-runSvgEff = execWriter <<< (flip runStateT initialSVG) <<< foldFree interpSvgEff
+runSVGEff :: ∀ a. Glyph a -> Array SVGElement
+runSVGEff = execWriter <<< (flip runStateT initialSVG) <<< foldFree interpSVGEff
 
 renderGlyph :: ∀ a eff. Glyph a -> Eff ( dom :: DOM | eff ) Element
-renderGlyph = SVG.renderSVG <<< runSvgEff
+renderGlyph = SVG.renderSVG <<< runSVGEff
