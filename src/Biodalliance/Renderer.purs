efb02061518598006db116d7a852d2263cbc3433
Christian Fischer <christian@chfi.se>
2017-03-03 14:21:58 +0100

    Cleanup & update LinePlot (still not quite working)

--- src/Biodalliance/Renderer.purs
+++
@@ -1,7 +0,0 @@
-module Biodalliance.Renderer
-       ( RendererConfig
-       ) where
-
--- TODO: this and many other types could be refactored into a Types module
-type RendererConfig c = { canvasHeight :: Number
-                        , yOffset :: Number | c }
--- src/Biodalliance/Renderer/Lineplot.purs
+++ src/Biodalliance/Renderer/Lineplot.purs
@@ -2,62 +2,45 @@ module Biodalliance.Renderer.Lineplot
        where
 
 import Prelude
-
-import Data.Array (tail, zipWith)
-import Data.Maybe (Maybe(..))
-
-import Biodalliance.Feature (Feature)
 import Biodalliance.Glyph.Free as Glyph
-
-import Biodalliance.Coordinates (CoordTransform)
-
-import Biodalliance.Renderer (RendererConfig)
-
+import Biodalliance.Feature (Feature(..), chrToScreen)
+import Biodalliance.Glyph.Free (Glyph, stroke, path)
+import Biodalliance.Glyph.Free.Interpret (writeGlyph)
+import Data.Array (tail, zipWith)
+import Data.Foreign (Foreign)
+import Data.Maybe (Maybe(..))
 
 type LineRow = (score :: Number)
 type LineFeature = Feature LineRow
 
 
 type LinePlotConfig =
-  RendererConfig ( minScore :: Number
-                 , maxScore :: Number
-                 , color :: String
-                 )
+  { minScore :: Number
+  , maxScore :: Number
+  , color :: String
+  }
 
 normalizeScore :: LinePlotConfig -> Number -> Number
 normalizeScore conf y = ((y - conf.minScore) / (conf.maxScore))
-
-
--- linePlotGlyph :: ∀ eff. CoordTransform
---                -> LinePlotConfig
---                -> Array LineFeature
---                -> Array (LineGlyph eff)
--- linePlotGlyph ct conf fs = gs
---   where fToPoint f = { x: f.min, y: normalizeScore conf f.score }
---         gs = case tail fs of
---           Nothing -> []
---           Just fs' -> zipWith (\f1 f2 -> Glyph.line (fToPoint f1) (fToPoint f2) ct f1)
---                       fs fs'
 
 type View = { viewStart :: Number
             , scale :: Number
             }
 
--- glyphifyFeatures :: ∀ eff.
---                     View
---                  -> Array (LineFeature)
---                  -> Array (LineGlyph eff)
--- glyphifyFeatures v fs = linePlotGlyph ct qtlPlotConfig fs
---   where ct = { h: { scale: v.scale, viewStart: v.viewStart }
---              , v: { height: 300.0 }
---              }
 
-glyphifyFeatures = id
+linePlot :: Array LineFeature -> Glyph Unit
+linePlot fs = do
+  stroke "#ff0000"
+  let ps = (\(Feature f) -> { x: f.min, y: f.max } ) <$> fs
+  path ps
+
+glyphifyFeatures :: View -> Array LineFeature -> Glyph Unit
+glyphifyFeatures v fs = linePlot fs
+
 
 
 qtlPlotConfig :: LinePlotConfig
 qtlPlotConfig = { minScore: 3.0
                 , maxScore: 5.0
-                , canvasHeight: 400.0
-                , yOffset: 0.0
-                , color: "#dd0000"}
+                , color: "#dd0000"
+                }
