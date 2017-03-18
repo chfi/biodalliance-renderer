99c6115441a969bcdd7d1de3b1bfccae16c81613
Christian Fischer <christian@chfi.se>
2017-03-18 12:50:12 +0100

    Rewrite GWAS feature parser & Glyph interpreter to use F; send errors as strings to BD.

--- src/Biodalliance/Glyph/Free/Interpret.purs
+++ src/Biodalliance/Glyph/Free/Interpret.purs
@@ -5,23 +5,36 @@ module Biodalliance.Glyph.Free.Interpret
 import Prelude
 import Biodalliance.Glyph.Free.Canvas as Canvas
 import Biodalliance.Glyph.Free.SVG as SVG
+import Biodalliance.Feature (Feature)
 import Biodalliance.Glyph.Free (Glyph)
 import Biodalliance.Glyph.Free.Position (glyphToGlyphPosition)
-import Biodalliance.Feature (Feature)
 import Control.Monad.Eff.Unsafe (unsafePerformEff)
-import Data.Foreign (Foreign, Prop(..), toForeign, writeObject)
+import Control.Monad.Except (runExcept)
+import Data.Either (Either(..))
+import Data.Foldable (fold)
+import Data.Foreign (F, Foreign, Prop(..), renderForeignError, toForeign, writeObject)
+import Data.Foreign.Null (writeNull)
+import Data.Maybe (Maybe, fromMaybe)
 import Data.Newtype (unwrap)
 
 
-writeGlyph :: ∀ a r. Feature r -> Glyph a -> Foreign
-writeGlyph f g = writeObject [ unsafeProp "draw" $ unsafePerformEff <<< \ctx -> Canvas.renderGlyph ctx g
+writeGlyph' :: ∀ a r. Maybe (Feature r) -> Glyph a -> Foreign
+writeGlyph' f g = writeObject [ unsafeProp "draw" $ unsafePerformEff <<< \ctx -> Canvas.renderGlyph ctx g
                              , unsafeProp "min" $ const p.min
                              , unsafeProp "max" $ const p.max
                              , unsafeProp "minY" $ const p.minY
                              , unsafeProp "maxY" $ const p.maxY
-                             , unsafeProp "feature" f
+                             , unsafeProp "feature" f'
                              , unsafeProp "toSVG" $ unsafePerformEff <<< \_ -> SVG.renderGlyph g
                              ]
     where p = unwrap $ glyphToGlyphPosition g
+          f' = fromMaybe writeNull (toForeign <$> f)
           unsafeProp :: ∀ x. String -> x -> Prop
           unsafeProp k v = Prop { key: k, value: toForeign v }
+
+
+
+writeGlyph :: ∀ a r. Maybe (Feature r) -> F (Glyph a) -> Foreign
+writeGlyph f fG = case runExcept fG of
+  Left errors -> toForeign $ fold $ renderForeignError <$> errors
+  Right glyph -> writeGlyph' f glyph
--- src/Biodalliance/Renderer/GWAS.purs
+++ src/Biodalliance/Renderer/GWAS.purs
@@ -3,31 +3,32 @@ module Biodalliance.Renderer.GWAS
        ) where
 
 import Prelude
+import Data.Foreign.Class
+import Data.Foreign.Generic
+import Data.Generic.Rep
 import Math as Math
 import Biodalliance.Feature (Feature(Feature), chrToScreen)
 import Biodalliance.Glyph.Free (Glyph, circle, fill, stroke)
 import Biodalliance.Glyph.Free.Interpret (writeGlyph)
+import Control.Error.Util (hush)
 import Control.Monad.Except (runExcept)
-import Control.MonadZero (empty)
-import Data.Array (mapMaybe)
-import Data.Either (Either(..))
-import Data.Foreign (Foreign, F)
-import Data.Foreign.Class (readProp)
-import Data.Maybe (Maybe(..))
+import Data.Foreign (F, Foreign, readString, toForeign)
+import Data.Foreign.Index (prop)
+import Global (readFloat)
 
 type GWASRow = (score :: Number)
 type GWASFeature = Feature GWASRow
 
+readGWASFeature :: Foreign -> F GWASFeature
+readGWASFeature f = do
+  fMin <- readProp "min" f
+  fMax <- readProp "max" f
+  score <- prop "pValue" f >>= readString <#> readFloat
+  pure $ Feature { min: fMin, max: fMax, score: score }
+
 type View = { viewStart :: Number
             , scale :: Number
             }
-
-foreign import parseFeatureImpl :: ∀ a. (a -> Maybe a) -> Maybe a -> Foreign -> Maybe GWASFeature
-
--- TODO: improve errors; Except would be good here
--- however - how to communicate errors to BD?
-parseFeature :: Foreign -> Maybe GWASFeature
-parseFeature = parseFeatureImpl Just Nothing
 
 glyphifyFeature :: String -> GWASFeature -> Glyph Unit
 glyphifyFeature col (Feature f) = do
@@ -35,10 +36,12 @@ glyphifyFeature col (Feature f) = do
   fill col
   circle { x: f.min, y: 300.0 + 10.0 * (Math.log f.score / Math.log 10.0) } 3.0
 
-featureToForeign :: View -> GWASFeature -> Foreign
+featureToForeign :: View -> F GWASFeature -> Foreign
 featureToForeign v f =
-  let g = glyphifyFeature "#2222dd" (chrToScreen v.scale v.viewStart f)
-  in writeGlyph f g
+  let tf = (chrToScreen v.scale v.viewStart) <$> f
+      g = glyphifyFeature "#2222dd" <$> tf
+      f' = hush $ runExcept f
+  in writeGlyph f' g
 
-glyphifyFeatures :: View -> Array Foreign -> Array Foreign
-glyphifyFeatures v fs = mapMaybe (\f -> (pure <$> featureToForeign v) =<< parseFeature f) fs
+glyphifyFeatures :: View -> Array Foreign -> Foreign
+glyphifyFeatures v fs = toForeign $ featureToForeign v <<< readGWASFeature <$> fs
